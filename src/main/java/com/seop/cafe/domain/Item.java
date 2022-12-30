a319a297dd826f663f7b967c1633ee13edbfa093
tseop <77163842+tseop@users.noreply.github.com>
2022-12-30 14:30:53 +0900

    feat: Create item domain

---
+++ src/main/java/com/seop/cafe/domain/Item.java
@@ -0,0 +1,26 @@
+package com.seop.cafe.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+    Long no;
+    String name;
+    String category;
+    int price;
+    int stock;
+
+    public void updatePrice(int price) {
+        this.price = price;
+    }
+
+    public void inStock(int count) {
+        stock += count;
+    }
+
+    public void outStock(int count) {
+        stock -= count;
+    }
+}

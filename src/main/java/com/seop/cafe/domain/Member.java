623eceff4c5e76397b6498d256799d0c6b6d9dbc
tseop <77163842+tseop@users.noreply.github.com>
2022-12-30 13:14:13 +0900

    feat: Create Member domain

---
+++ src/main/java/com/seop/cafe/domain/Member.java
@@ -0,0 +1,16 @@
+package com.seop.cafe.domain;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    Long no;
+    String name;
+    int age;
+    LocalDateTime regiDate;
+    LocalDateTime lastUseDate;
+    String gender;
+}

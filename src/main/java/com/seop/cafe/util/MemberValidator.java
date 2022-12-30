add44e2ed4675a0c8fecc6869ca421bd50843c98
tseop <77163842+tseop@users.noreply.github.com>
2022-12-30 16:08:15 +0900

    feat: Create Member Register Validator

---
+++ src/main/java/com/seop/cafe/util/MemberValidator.java
@@ -0,0 +1,46 @@
+package com.seop.cafe.util;
+
+import com.seop.cafe.domain.Member;
+
+public class MemberValidator {
+    private static final int MEMBER_NAME_MAX_LENS = 5;
+    private static final int MEMBER_AGE_MIN_VALUE = 3;
+    private static final int MEMBER_AGE_MAX_VALUE = 200;
+    private Member member;
+
+    public void registerValidate(Member member) {
+        this.member = member;
+        nameValidate();
+        ageValidate();
+        genderValidate();
+    }
+
+    private void nameValidate() {
+        nameLenZero();
+        nameLensValidate();
+    }
+
+    private void nameLenZero() {
+        if (member.getName().length() == 0) {
+            throw new IllegalArgumentException("이름을 입력해주세요.");
+        }
+    }
+
+    private void nameLensValidate() {
+        if (member.getName().length() > MEMBER_NAME_MAX_LENS) {
+            throw new IllegalArgumentException("이름이 너무 길어요.");
+        }
+    }
+
+    private void ageValidate() {
+        if (member.getAge() < MEMBER_AGE_MIN_VALUE || member.getAge() < MEMBER_AGE_MAX_VALUE) {
+            throw new IllegalArgumentException("가입이 불가능한 나이입니다.");
+        }
+    }
+
+    private void genderValidate() {
+        if (member.getGender() == null) {
+            throw new IllegalArgumentException("성별을 입력해 주세요.");
+        }
+    }
+}

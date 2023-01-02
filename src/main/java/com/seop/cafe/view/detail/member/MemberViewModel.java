d0e78d059ceee1eff40591f90f588b2318f26a52
tseop <77163842+tseop@users.noreply.github.com>
2023-01-02 16:59:58 +0900

    feat: Create MVC pattern View model

---
+++ src/main/java/com/seop/cafe/view/detail/member/MemberViewModel.java
@@ -0,0 +1,34 @@
+package com.seop.cafe.view.detail.member;
+
+import com.seop.cafe.view.Viewable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberViewModel {
+    private final String viewPath;
+    private final Map<String, Object> model;
+
+    public MemberViewModel(String viewPath) {
+        this.viewPath = viewPath;
+        this.model = new HashMap<>();
+    }
+
+    public MemberViewModel(String viewPath, Map<String, Object> model) {
+        this.viewPath = viewPath;
+        this.model = model;
+    }
+
+    public void add(String key, Object object) {
+        model.put(key, object);
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void render() {
+        MemberViewFactory viewFactory = MemberViewFactory.getInstance();
+        Viewable viewable = viewFactory.get(viewPath);
+        viewable.render(model);
+    }
+}

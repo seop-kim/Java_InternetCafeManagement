a951f446dc1b9332f655b1dabcaf794469d31024
tseop <77163842+tseop@users.noreply.github.com>
2023-01-02 16:50:49 +0900

    feat: Create TestData init

---
+++ src/main/java/com/seop/cafe/MemberTestData.java
@@ -0,0 +1,19 @@
+package com.seop.cafe;
+
+import com.seop.cafe.domain.Gender;
+import com.seop.cafe.domain.Member;
+import com.seop.cafe.domain.service.MemberService;
+
+public class MemberTestData {
+    private final static MemberService service = new MemberService();
+
+    public static void init() {
+        for (int i = 0; i < 10; i++) {
+            Member member = new Member();
+            member.setName("mem" + i);
+            member.setAge(10 + i);
+            member.setGender(Gender.MALE);
+            service.register(member);
+        }
+    }
+}

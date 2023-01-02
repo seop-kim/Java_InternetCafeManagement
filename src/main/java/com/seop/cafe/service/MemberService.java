81c87260aff90e341915c23dfbc8fc3b156ca15d
tseop <77163842+tseop@users.noreply.github.com>
2023-01-02 18:10:29 +0900

    feat: Create Member delete validate

--- src/main/java/com/seop/cafe/service/MemberService.java
+++ src/main/java/com/seop/cafe/service/MemberService.java
@@ -4,10 +4,14 @@ import com.seop.cafe.domain.Member;
 import com.seop.cafe.repository.MemberRepository;
 import com.seop.cafe.util.MemberValidator;
 import java.util.List;
+import java.util.stream.Stream;
+import javax.xml.stream.StreamFilter;
 
 public class MemberService {
     private final MemberRepository repository = MemberRepository.getInstance();
     private final MemberValidator validator = new MemberValidator();
+
+    private static final String MEMBER_VALIDATE_DELETE_EXCEPTION_MSG = "존재하지 않는 회원입니다.";
 
     public Member register(Member member) {
         validator.registerValidate(member);
@@ -27,6 +31,15 @@ public class MemberService {
     }
 
     public void delete(Long no) {
+        deleteValidate(no);
         repository.delete(no);
+    }
+
+
+    private void deleteValidate(Long no) {
+        Member findMember = repository.findOne(no);
+        if (findMember == null) {
+            throw new IllegalArgumentException(MEMBER_VALIDATE_DELETE_EXCEPTION_MSG);
+        }
     }
 }

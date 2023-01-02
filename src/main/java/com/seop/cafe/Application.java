880fe081a9e33ce873fe306ded17a37dc7cb0984
tseop <77163842+tseop@users.noreply.github.com>
2023-01-02 17:03:10 +0900

    refactor: Modify main method

--- src/main/java/com/seop/cafe/Application.java
+++ src/main/java/com/seop/cafe/Application.java
@@ -1,8 +1,17 @@
 package com.seop.cafe;
 
+import static com.seop.cafe.MemberTestData.*;
+
+import com.seop.cafe.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("GOOD LUCK!");
         // TODO
+        init();
+        MainController controller = new MainController();
+        while (true) {
+            controller.run();
+        }
     }
 }

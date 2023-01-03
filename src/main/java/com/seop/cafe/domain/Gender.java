7e823af969c692fac7cb6ee598c116f4aa6f0c85
tseop <77163842+tseop@users.noreply.github.com>
2023-01-03 17:54:03 +0900

    refactor: Create Gender getGender

--- src/main/java/com/seop/cafe/domain/Gender.java
+++ src/main/java/com/seop/cafe/domain/Gender.java
@@ -1,16 +1,28 @@
 package com.seop.cafe.domain;
 
 public enum Gender {
-    MALE("남성"),
-    FEMALE("여성");
+    MALE("1", "남성"),
+    FEMALE("2", "여성");
 
+    private final String no;
     private final String gender;
 
-    Gender(String gender) {
+
+    Gender(String no, String gender) {
+        this.no = no;
         this.gender = gender;
     }
 
     public String getGender(Gender gender) {
         return this.gender;
+    }
+
+    public static Gender getGender(String no) {
+        for (Gender findGender : Gender.values()) {
+            if (findGender.no.equals(no)) {
+                return findGender;
+            }
+        }
+        throw new IllegalArgumentException("찾는 성별이 없습니다.");
     }
 }

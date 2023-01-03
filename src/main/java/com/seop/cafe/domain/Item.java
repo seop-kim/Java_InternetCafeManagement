360e12ab6d22d05a1fa65da988958a888316b7b3
tseop <77163842+tseop@users.noreply.github.com>
2023-01-03 15:02:28 +0900

    refactor: Create Item class update method validate

--- src/main/java/com/seop/cafe/domain/Item.java
+++ src/main/java/com/seop/cafe/domain/Item.java
@@ -13,6 +13,7 @@ public class Item {
     int stock;
 
     public void updatePrice(int price) {
+        priceValidate(price);
         this.price = price;
     }
 
@@ -21,6 +22,19 @@ public class Item {
     }
 
     public void outStock(int count) {
+        stockValidate(count);
         stock -= count;
+    }
+
+    private void priceValidate(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("0원보다 작은 값을 입력할 수 없습니다.");
+        }
+    }
+
+    private void stockValidate(int count) {
+        if (stock - count < 0) {
+            throw new IllegalArgumentException("현재 재고를 초과한 수량을 내보낼 수 없습니다.");
+        }
     }
 }

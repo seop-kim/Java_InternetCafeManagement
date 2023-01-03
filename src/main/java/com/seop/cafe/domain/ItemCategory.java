bcca98011a327155e997cb8a12fe84d41bd69cd5
tseop <77163842+tseop@users.noreply.github.com>
2023-01-03 18:01:31 +0900

    refactor: Add ItemCategory getCategory method

--- src/main/java/com/seop/cafe/domain/ItemCategory.java
+++ src/main/java/com/seop/cafe/domain/ItemCategory.java
@@ -1,16 +1,29 @@
 package com.seop.cafe.domain;
 
 public enum ItemCategory {
-    SNACK("과자"),
-    DRINK("음료"),
-    RAMEN("라면");
+    SNACK("1", "과자"),
+    DRINK("2", "음료"),
+    RAMEN("3", "라면");
+
+    private final String no;
 
     private final String category;
-    ItemCategory(String category) {
+
+    ItemCategory(String no, String category) {
+        this.no = no;
         this.category = category;
     }
 
     public String getCategory() {
         return this.category;
+    }
+
+    public static ItemCategory getCategory(String no) {
+        for (ItemCategory findCate : ItemCategory.values()) {
+            if (findCate.no.equals(no)) {
+                return findCate;
+            }
+        }
+        throw new IllegalArgumentException("없는 카데고리 입니다.");
     }
 }

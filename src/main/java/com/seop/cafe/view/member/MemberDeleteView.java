3093e8153804a7d5ea8b2ae290492649055dffd6
tseop <77163842+tseop@users.noreply.github.com>
2023-01-03 18:00:28 +0900

    style: Modify constant declaration

--- src/main/java/com/seop/cafe/view/member/MemberDeleteView.java
+++ src/main/java/com/seop/cafe/view/member/MemberDeleteView.java
@@ -4,10 +4,10 @@ import com.seop.cafe.view.Viewable;
 import java.util.Map;
 
 public class MemberDeleteView implements Viewable {
-    private static final String MEMBER_DELETE_USER_MSG = "회원 정보가 삭제 되었습니다.";
+    private static final String MEMBER_DELETE_COMP_MSG = "회원 정보가 삭제 되었습니다.";
 
     @Override
     public void render(Map<String, Object> model) {
-        System.out.println(MEMBER_DELETE_USER_MSG);
+        System.out.println(MEMBER_DELETE_COMP_MSG);
     }
 }

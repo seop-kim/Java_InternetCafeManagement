41eceb881a30502fca6094975f955ccd8d52212c
tseop <77163842+tseop@users.noreply.github.com>
2023-01-04 09:59:41 +0900

    refactor: Change Init data

--- src/main/java/com/seop/cafe/init/ItemTestData.java
+++ src/main/java/com/seop/cafe/init/ItemTestData.java
@@ -1,5 +1,7 @@
 package com.seop.cafe.init;
 
+import static com.seop.cafe.domain.ItemCategory.*;
+
 import com.seop.cafe.domain.Item;
 import com.seop.cafe.domain.ItemCategory;
 import com.seop.cafe.service.ItemService;
@@ -7,13 +9,18 @@ import com.seop.cafe.service.ItemService;
 public class ItemTestData {
     private static final ItemService service = new ItemService();
 
+    private static String[] names = {"신라면", "안성탕면", "코카콜라", "펩시콜라", "사이다", "썬칩", "감자칩", "건빵", "생수", "바나나우유"};
+    private static ItemCategory[] category = {RAMEN, RAMEN, DRINK, DRINK, DRINK, SNACK, SNACK, SNACK, DRINK, DRINK};
+    private static int[] price = {2500, 2500, 1500, 1500, 1500, 2000, 2000, 1000, 800, 1400};
+    private static int[] stock = {50, 50, 100, 100, 80, 30, 30, 10, 100, 50};
+
     public static void init() {
         for (int i = 0; i < 10; i++) {
             Item item = new Item();
-            item.setName("item" + i);
-            item.setCategory(ItemCategory.SNACK);
-            item.setPrice(5000 + i);
-            item.setStock(100 + i);
+            item.setName(names[i]);
+            item.setCategory(category[i]);
+            item.setPrice(price[i]);
+            item.setStock(stock[i]);
             service.add(item);
         }
     }
--- src/main/java/com/seop/cafe/init/MemberTestData.java
+++ src/main/java/com/seop/cafe/init/MemberTestData.java
@@ -1,18 +1,24 @@
 package com.seop.cafe.init;
 
+import static com.seop.cafe.domain.Gender.*;
+
 import com.seop.cafe.domain.Gender;
 import com.seop.cafe.domain.Member;
 import com.seop.cafe.service.MemberService;
+import java.util.List;
 
 public class MemberTestData {
     private final static MemberService service = new MemberService();
+    private static String[] names = {"김태섭", "성명건", "정승민", "윤현민", "김분도", "이동준", "최태영", "최고윤", "홍진혁", "박봉순"};
+    private static int[] ages = {29, 28, 34, 31, 36, 40, 39, 40, 29, 45};
+    private static Gender[] genders = {MALE, MALE, MALE, MALE, MALE, MALE, MALE, MALE, MALE, FEMALE};
 
     public static void init() {
         for (int i = 0; i < 10; i++) {
             Member member = new Member();
-            member.setName("mem" + i);
-            member.setAge(10 + i);
-            member.setGender(Gender.MALE);
+            member.setName(names[i]);
+            member.setAge(ages[i]);
+            member.setGender(genders[i]);
             service.register(member);
         }
     }

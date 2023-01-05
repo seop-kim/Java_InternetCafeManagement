000d1a767f52785e4404cf5f7ee054bd4c01ff60
tseop <77163842+tseop@users.noreply.github.com>
2023-01-05 13:53:21 +0900

    feat: Update default method in Adapter interface

--- src/main/java/com/seop/cafe/adapter/IAdapter.java
+++ src/main/java/com/seop/cafe/adapter/IAdapter.java
@@ -1,9 +1,20 @@
 package com.seop.cafe.adapter;
 
 import com.seop.cafe.view.ViewModel;
+import java.util.Map;
 
 public interface IAdapter {
     boolean isSupport(Object controller);
 
     ViewModel process(Object controller);
+
+    /**
+     * 뷰 -> 컨트롤러 데이터 전달을 위한 default method
+     *
+     * @param controller
+     * @param model
+     */
+    default ViewModel process(Object controller, Map<String, Object> model) {
+        return null;
+    }
 }

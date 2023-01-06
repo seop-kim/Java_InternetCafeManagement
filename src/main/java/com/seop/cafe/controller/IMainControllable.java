65c2b5f2fd78e148c617cf291856cc87c10b65e0
tseop <77163842+tseop@users.noreply.github.com>
2023-01-06 17:25:28 +0900

    refactor: Update Interface default method

--- src/main/java/com/seop/cafe/controller/IMainControllable.java
+++ src/main/java/com/seop/cafe/controller/IMainControllable.java
@@ -1,6 +1,12 @@
 package com.seop.cafe.controller;
 
+import java.util.Map;
+
 @FunctionalInterface
 public interface IMainControllable {
     void process();
+
+    default void mapping(String path, Map<String, Object> model) {
+
+    }
 }

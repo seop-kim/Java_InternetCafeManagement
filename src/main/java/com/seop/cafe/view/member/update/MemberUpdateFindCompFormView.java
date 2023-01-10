74289c594598e755ec343c730f96b7df67d42c7b
tseop <77163842+tseop@users.noreply.github.com>
2023-01-10 11:32:48 +0900

    style: Add \n

--- src/main/java/com/seop/cafe/view/member/update/MemberUpdateFindCompFormView.java
+++ src/main/java/com/seop/cafe/view/member/update/MemberUpdateFindCompFormView.java
@@ -21,7 +21,7 @@ public class MemberUpdateFindCompFormView extends ViewForm implements Viewable {
 
     @Override
     protected void setup() {
-        VIEW_FORM_TEXT = "어떤 항목을 수정 하시겠습니까?\t1 이름\t2 나이\n";
+        VIEW_FORM_TEXT = "\n어떤 항목을 수정 하시겠습니까?\t1 이름\t2 나이\n";
         CONTROLLER = "MEMBER";
         CONTROLLER_PATH = "MEMBER_UPDATE_SELECT_HEAD";
         KEY = "pick";

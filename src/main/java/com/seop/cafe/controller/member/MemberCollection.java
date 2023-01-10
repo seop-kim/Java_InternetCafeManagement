17f511f6a52ab442f54c086e1f51ad33a2ff8839
tseop <77163842+tseop@users.noreply.github.com>
2023-01-10 16:17:45 +0900

    style: Remove comments

--- src/main/java/com/seop/cafe/controller/member/MemberCollection.java
+++ src/main/java/com/seop/cafe/controller/member/MemberCollection.java
@@ -2,7 +2,6 @@ package com.seop.cafe.controller.member;
 
 import com.seop.cafe.controller.common.EndFunction;
 import com.seop.cafe.function.member.MemberSelectForm;
-//import com.seop.cafe.function.member.MemberSelectFunction;
 import com.seop.cafe.function.member.delete.MemberDeleteForm;
 import com.seop.cafe.function.member.delete.MemberDeleteFunction;
 import com.seop.cafe.function.member.find.MemberFindAllFunction;
@@ -16,22 +15,20 @@ import com.seop.cafe.function.member.update.MemberUpdateFunction;
 import com.seop.cafe.function.member.update.MemberUpdateSelectHeadForm;
 
 public enum MemberCollection {
-    MEMBER_SELECT_FORM("MEMBER_SELECT_FORM", new MemberSelectForm()),
-//    MEMBER_SELECT_FUNCTION("MEMBER_SELECT_FUNCTION", new MemberSelectFunction()),
-    MEMBER_REGISTER_FORM("1", new MemberRegisterForm()),
-    MEMBER_FIND_ALL_FUNCTION("2", new MemberFindAllFunction()),
-    MEMBER_FIND_NAME_FORM("3", new MemberNameFindForm()),
-    MEMBER_UPDATE_FIND_FORM("4", new MemberUpdateFindForm()),
-    MEMBER_DELETE_FIND_FORM("5", new MemberDeleteForm()),
-    END("0", new EndFunction()),
+    MEMBER_SELECT_REGISTER("1", new MemberRegisterForm()),
+    MEMBER_SELECT_FIND_ALL("2", new MemberFindAllFunction()),
+    MEMBER_SELECT_FIND_NAME("3", new MemberNameFindForm()),
+    MEMBER_SELECT_UPDATE("4", new MemberUpdateFindForm()),
+    MEMBER_SELECT_DELETE("5", new MemberDeleteForm()),
+    MEMBER_SELECT_BACK("0", new EndFunction()),
 
+    MEMBER_SELECT_FORM("MEMBER_SELECT_FORM", new MemberSelectForm()),
     MEMBER_REGISTER_FUNCTION("MEMBER_REGI_FUNCTION", new MemberRegisterFunction()),
     MEMBER_FIND_NAME_FUNCTION("MEMBER_SEARCH_NAME_FUNCTION", new MemberFindNameFunction()),
     MEMBER_DELETE_FUNCTION("MEMBER_DELETE_FUNCTION", new MemberDeleteFunction()),
     MEMBER_UPDATE_FIND_COMP_FORM("MEMBER_UPDATE_FIND_COMP_FORM", new MemberUpdateFindCompForm()),
     MEMBER_UPDATE_SELECT_HEAD("MEMBER_UPDATE_SELECT_HEAD", new MemberUpdateSelectHeadForm()),
     MEMBER_UPDATE_FUNCTION("MEMBER_UPDATE_FUNCTION", new MemberUpdateFunction());
-
 
     private final String path;
     private final Object function;
